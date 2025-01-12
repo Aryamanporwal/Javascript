bc5022ee48e7add9a9f4dba64f69e2a44b18983f
Aryamanporwal <aryaman.gla_cs23@gla.ac.in>
2025-01-12 20:11:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3)  //we are using node js not browser
+
+console.log(3+3)
+
+
+let name = "aryaman"
+let age = 20
+let isloggedIn = false
+let state = null
+
+// number => 2 to 53
+// bigint
+// string => ""
+// boolean => true/false
+// null (stand alone value) type of null = object
+// undefined => (jiski value abhi na di ho)
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined ); ///undefined
+console.log(typeof null); //object
